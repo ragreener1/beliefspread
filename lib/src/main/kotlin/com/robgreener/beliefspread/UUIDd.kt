433df351bdc7e4b175c351d5460391f82a6f1e24
Robert Greener <rob@robgreener.com>
2022-04-13 11:52:56 +0100

    Change UUIDd

--- lib/src/main/kotlin/com/robgreener/beliefspread/UUIDd.kt
+++ lib/src/main/kotlin/com/robgreener/beliefspread/UUIDd.kt
@@ -4,13 +4,6 @@ import java.util.UUID
 
 /** Something with a UUID. */
 public interface UUIDd {
-    /** Gets the UUID of the Object. */
-    fun getUuid(): UUID
-
-    /**
-     * Sets the UUID of the object.
-     *
-     * @param uuid The new UUID.
-     */
-    fun setUuid(uuid: UUID)
+    /** The UUID of the Object. */
+    var uuid: UUID
 }

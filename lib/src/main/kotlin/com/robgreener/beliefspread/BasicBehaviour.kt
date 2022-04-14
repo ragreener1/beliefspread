58163b45d9a353c32517bff0b135fe8c1b00710c
Robert Greener <rob@robgreener.com>
2022-04-14 10:38:58 +0100

    Add GPL-3 to files

--- lib/src/main/kotlin/com/robgreener/beliefspread/BasicBehaviour.kt
+++ lib/src/main/kotlin/com/robgreener/beliefspread/BasicBehaviour.kt
@@ -1,6 +1,24 @@
+/*
+ * belief-spread
+ * Copyright (c) 2022 Robert Greener
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the LICENSE, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public LICENSE
+ * along with this program. If not, see <https://www.gnu.org/licenses>
+ */
+
 package com.robgreener.beliefspread
 
-import java.util.UUID
+import java.util.*
 
 /**
  * A BasicBehaviour is an implementation of Behaviour.
@@ -14,5 +32,5 @@ class BasicBehaviour(override var name: String, override var uuid: UUID) : Behav
      *
      * @param name The name of the BasicBehaviour.
      */
-    constructor(name: String) : this(name, UUID.randomUUID()) {}
+    constructor(name: String) : this(name, UUID.randomUUID())
 }

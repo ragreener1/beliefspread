7f27bfc7d483ee62c796409e672fe77bdb425f00
Robert Greener <rob@robgreener.com>
2022-04-21 09:38:08 +0100

    Add BasicBDIAgent with constructors

---
+++ lib/src/main/kotlin/io/github/ragreener1/beliefspread/bdi/BasicBDIAgent.kt
@@ -0,0 +1,51 @@
+/*
+ * belief-spread
+ * Copyright (c) 2022 Robert Greener
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the LICENSE, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public LICENSE
+ * along with this program. If not, see <https://www.gnu.org/licenses>
+ */
+
+package io.github.ragreener1.beliefspread.bdi
+
+import io.github.ragreener1.beliefspread.core.BasicAgent
+import io.github.ragreener1.beliefspread.core.Belief
+import io.github.ragreener1.beliefspread.core.UnstableAPI
+import java.util.*
+
+/**
+ * A [BasicBDIAgent] is an implementation of [BDIAgent], extending from [BasicAgent].
+ *
+ * @param uuid The [UUID] of the agent.
+ * @constructor Create a new [BasicBDIAgent] with a [uuid].
+ * @author Robert Greener
+ * @since v0.13.0
+ */
+@UnstableAPI
+open class BasicBDIAgent(uuid: UUID) : BasicAgent(uuid), BDIAgent {
+    /**
+     * Create a new [BasicBDIAgent] with a random [UUID].
+     *
+     * The [UUID] is generated with [UUID.randomUUID]
+     * @author Robert Greener
+     * @since v0.13.0
+     */
+    @UnstableAPI
+    constructor() : this(UUID.randomUUID())
+
+    @UnstableAPI
+    @Deprecated("Not yet implemented", level = DeprecationLevel.ERROR)
+    override fun chooseAction(time: UInt, beliefs: Collection<Belief>) {
+        TODO("Not yet implemented")
+    }
+}
